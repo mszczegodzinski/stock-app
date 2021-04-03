04252df83191a882db6a112992911d07315f52e7
mszczegodzinski <mszczegodzinski@o2.pl>
2021-04-04 01:50:28 +0200

    Updates

---
+++ precache-manifest.0aff6a9517a80ababe3a590656a1d63c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ade870d1f9c6aafa31b6f2013f036d",
+    "url": "/stock-app/index.html"
+  },
+  {
+    "revision": "4d022df3416c0de027af",
+    "url": "/stock-app/static/css/main.58421e38.chunk.css"
+  },
+  {
+    "revision": "6d420fd4c56fd622fb65",
+    "url": "/stock-app/static/js/2.eb592cfa.chunk.js"
+  },
+  {
+    "revision": "94b51f2ba7e7af11e2002fb8012b613c",
+    "url": "/stock-app/static/js/2.eb592cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d022df3416c0de027af",
+    "url": "/stock-app/static/js/main.d52a2db5.chunk.js"
+  },
+  {
+    "revision": "7e73c23810d18a316edb",
+    "url": "/stock-app/static/js/runtime-main.5d6de826.js"
+  }
+]);
